4202f8190c02a7080728c16324eaba01c61fa76e
Joonwoo Park <joonwoop@codeaurora.org>
2017-03-07 18:08:24 -0800

    sched: EAS: kill incorrect nohz idle cpu kick
    EAS won't allow NOHZ idle balancer until CPU's over utilized.  However
    nohz_kick_needed() can return true.  This causes idle CPU wake up for
    nothing.
    
    Change-Id: I6e548442e29e4f85cda695e4c7101dd591b12fe6
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9511,9 +9511,13 @@ static inline bool nohz_kick_needed(struct rq *rq)
 	    (!energy_aware() || cpu_overutilized(cpu)))
 		return true;
 
+	/* Do idle load balance if there have misfit task */
+	if (energy_aware())
+		return rq->misfit_task;
+
 	rcu_read_lock();
 	sd = rcu_dereference(per_cpu(sd_busy, cpu));
-	if (sd && !energy_aware()) {
+	if (sd) {
 		sgc = sd->groups->sgc;
 		nr_busy = atomic_read(&sgc->nr_busy_cpus);
 
