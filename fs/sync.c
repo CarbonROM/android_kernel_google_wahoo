e06fe6595ff6546deaac06feea337eafa1cb601c
RJ Trujillo <certifiedblyndguy@gmail.com>
2018-08-13 16:21:16 -0600

    fs: sync: Disable fsync on screen-off
    It seems rather pointless to waste resources when we rarely, if
    at all see fsync posing a real use when a device equipped with
    UFS capabilities enters suspend.
    
    Change-Id: I620ec24e34569cb772b9a282085c79d7c76a849c
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -16,6 +16,7 @@
 #include <linux/pagemap.h>
 #include <linux/quotaops.h>
 #include <linux/backing-dev.h>
+#include <linux/state_notifier.h>
 #include "internal.h"
 
 bool fsync_enabled = true;
@@ -161,7 +162,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct super_block *sb;
 	int ret;
 
-	if (!fsync_enabled)
+	if (!fsync_enabled || state_suspended)
 		return 0;
 
 	f = fdget(fd);
@@ -192,7 +193,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
 
-	if (!fsync_enabled)
+	if (!fsync_enabled || state_suspended)
 		return 0;
 
 	if (!file->f_op->fsync)
@@ -226,7 +227,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	struct fd f;
 	int ret = -EBADF;
 
-	if (!fsync_enabled)
+	if (!fsync_enabled || state_suspended)
 		return 0;
 
 	f = fdget(fd);
@@ -304,7 +305,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
 
-	if (!fsync_enabled)
+	if (!fsync_enabled || state_suspended)
 		return 0;
 
 	ret = -EINVAL;
